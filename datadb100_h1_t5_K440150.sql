7b3e69f6e80fdbafe5bec60ef944036bd102759b
Md. Abdullah-Al Mamun <mamun.cou13@gmail.com>
2021-04-05 09:53:25 +0300

    Add files via upload

---
+++ datadb100_h1_t5_K440150.sql
@@ -0,0 +1,14 @@
+-- Tietokantojen perusteet - Database basics 2021
+-- H1 T5
+-- abdullah-al.mamun@tuni.fi 
+
+SELECT ar.artwork_id, ar.artwork_name, ar.technique
+FROM artwork AS ar
+INNER JOIN displayed_at ON displayed_at.artwork_id = ar.artwork_id 
+INNER JOIN exhibition ON displayed_at.exhibition_id = exhibition.exhibition_id
+WHERE exhibition.location = "Museum of Modern Art"
+ORDER BY ar.artwork_id ASC;
+
+
+
+
